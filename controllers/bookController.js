8604dbc5145e75de0e393b80f40359419aa980f4
David Stockdale <dstock3@gmail.com>
2022-03-24 17:50:17 -0500

    Added notations to async.parallel function within bookController.js.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,6 +8,8 @@ var async = require('async');
 exports.index = function(req, res) {
 
     async.parallel({
+        //The async.parallel() method is passed an object with functions for getting the counts for each of our models. These functions are all started at the same time. When all of them have completed the final callback is invoked with the counts in the results parameter (or an error).
+
         book_count: function(callback) {
             Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
         },
@@ -24,6 +26,9 @@ exports.index = function(req, res) {
             Genre.countDocuments({}, callback);
         }
     }, function(err, results) {
+        //On success the callback function calls res.render() specifying a view (template) named 'index' and an object containing the data that is to be inserted into it (this includes the results object that contains our model counts). The data is supplied as key-value pairs, and can be accessed in the template using the key.
+
+        //Note: The callback function from async.parallel() above is a little unusual in that we render the page whether or not there was an error (normally you might use a separate execution path for handling the display of errors).
         res.render('index', { title: 'Local Library Home', error: err, data: results });
     });
 };

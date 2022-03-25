2a0f4449a99faa7a43554cdf318066a28b9087ba
David Stockdale <dstock3@gmail.com>
2022-03-25 12:55:16 -0500

    Added contollers to handle author creation get and post requests. Added respective pug template file to views.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -56,15 +56,60 @@ exports.author_detail = function(req, res, next) {
 
 };
 
-//Display Author create form on GET
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+// Display Author create form on GET.
+//This renders the author_form.pug view, passing a title variable.
+exports.author_create_get = function(req, res, next) {
+    res.render('author_form', { title: 'Create Author'});
 };
 
-//Handle Author create on POST
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+// Handle Author create on POST.
+//First we validate and sanitize the data. If the data is invalid then we re-display the form along with the data that was originally entered by the user and a list of error messages. If the data is valid then we save the new author record and redirect the user to the author detail page.
+
+exports.author_create_post = [
+
+    // Validate and sanitize fields.
+    //We can daisy chain validators, using withMessage() to specify the error message to display if the previous validation method fails. This makes it very easy to provide specific error messages without lots of code duplication.
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+    //We can use the optional() function to run a subsequent validation only if a field has been entered (this allows us to validate optional fields).  (see above)
+    //Parameters are received from the request as strings. We can use toDate() (or toBoolean()) to cast these to the proper JavaScript types (as shown at the end of the validator chain above).
+
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Create an Author object with escaped and trimmed data.
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
 //Handle Author delete form on GET
 exports.author_delete_get = function(req, res) {
